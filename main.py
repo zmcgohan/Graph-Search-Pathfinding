d868b63b2273837633e301b16571c0d6a0ac75c8
Machz <mcgohan.4@osu.edu>
2014-12-08 18:55:26 -0500

    Added: 	- Can turn off iterative deepening from being ran when using a 	  larger grid; no way to effectively lower time taken AND get optimal paths since there's 4 actions for EVERY PathNode and it'll just get WAY too big without much progress

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import sys
 from grid import Grid, RandomGrid
 from path import Path
 
-PRINT_GRID = True
+PRINT_GRID = False
+ITERATIVE_DEEPENING = False
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
@@ -18,7 +19,7 @@ if __name__ == '__main__':
 		sys.exit(1)
 	except Exception as e:
 		print e
-	rand_grid = RandomGrid(10,10,4,4)
+	rand_grid = RandomGrid(500,500,4,4)
 	print "Random grid created. Starting searches...\n"
 	while 1:
 		start_pos = rand_grid.get_random_position()
@@ -30,15 +31,18 @@ if __name__ == '__main__':
 			end_pos = rand_grid.get_random_position()
 			graph_search_path = Path(rand_grid, start_pos, end_pos)
 		bidirectional_path = Path(rand_grid, start_pos, end_pos, 1)
-		iterative_deepening_path = Path(rand_grid, start_pos, end_pos, 2)
+		if ITERATIVE_DEEPENING:
+			iterative_deepening_path = Path(rand_grid, start_pos, end_pos, 2)
 		if PRINT_GRID:
 			rand_grid.display_path(graph_search_path)
 			print
 			rand_grid.display_path(bidirectional_path)
-			rand_grid.display_path(iterative_deepening_path)
+			if ITERATIVE_DEEPENING:
+				rand_grid.display_path(iterative_deepening_path)
 		else:
 			print "Path found from {} to {}".format(start_pos, end_pos)
 			print "Graph search path length: {}; time taken: {}s".format(len(graph_search_path.path_positions) - 1, graph_search_path.time_taken)
 			print "Bidirectional path length: {}; time taken: {}s".format(len(bidirectional_path.path_positions) - 1, bidirectional_path.time_taken)
-			print "Iterative deepening path length: {}; time taken: {}s".format(len(iterative_deepening_path.path_positions) - 1, iterative_deepening_path.time_taken)
+			if ITERATIVE_DEEPENING:
+				print "Iterative deepening path length: {}; time taken: {}s".format(len(iterative_deepening_path.path_positions) - 1, iterative_deepening_path.time_taken)
 		raw_input()
